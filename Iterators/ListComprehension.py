e2d12d9d2f0c1687e1a5d98625f6e58147046936
Ashish Awasthy <jbpashu@gmail.com>
2019-06-12 18:37:15 +0530

    Create ListComprehension.py

---
+++ Iterators/ListComprehension.py
@@ -0,0 +1,4 @@
+#How would a list comprehension that produces a list of the first character of each string in doctor look like? Note that the list comprehension uses doc as the iterator variable. What will the output be?
+
+[doc[0] for doc in doctor]
+
