ffa1a14804ed1868f05edd2b767f63b49dba60b1
Ashish Awasthy <jbpashu@gmail.com>
2019-06-12 18:42:17 +0530

    Update ListComprehension.py

--- Iterators/ListComprehension.py
+++ Iterators/ListComprehension.py
@@ -1,4 +1,19 @@
 #How would a list comprehension that produces a list of the first character of each string in doctor look like? Note that the list comprehension uses doc as the iterator variable. What will the output be?
+doctor = ['house', 'cuddy', 'chase', 'thirteen', 'wilson']
 
 [doc[0] for doc in doctor]
+
+#You know that list comprehensions can be built over iterables. Given the following objects below, which of these can we build list comprehensions over?
+
+doctor = ['house', 'cuddy', 'chase', 'thirteen', 'wilson']
+
+range(50)
+
+underwood = 'After all, we are nothing more or less than what we choose to reveal.'
+
+jean = '24601'
+
+flash = ['jay garrick', 'barry allen', 'wally west', 'bart allen']
+
+valjean = 24601
 
